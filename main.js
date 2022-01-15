165191b40495c519f427132f08f4fbef5b8eeda0
makena <mduncan18@byui.edu>
2022-01-15 15:03:26 -0700

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-// Example of what needs to be added
-// const links = [
-//  {
-//    label: "week 1 notes,"
-//    url: "week1/index.html"
-//  }
-// ]
+// Portfolio Table of Contents
+const contentsList = [
+    {
+        "label": "Week 1 Notes",
+        "url": "week01/index.html"
+    }
+]
 
+function createTable() {
+    var list = document.getElementById(container)
+
+    var li
+
+    for (var i = 0; i < contentsList.length; i++); {
+        li = document.createElement("li");
+        li.appendChild(document.createTextNode(contentsList[i]))
+        list.appendChild(li)
+    }
+
+    window.onload = createTable;
+}
